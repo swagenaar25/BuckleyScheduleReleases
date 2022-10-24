10b55e58a14f4ad3dc19c349901023abcf5c63b2
swagenaar25 <swagenaar@buckley.org>
2022-10-24 10:05:37 -0700

    Bit of debug info

--- update_latest.py
+++ update_latest.py
@@ -5,6 +5,7 @@ ver = sys.argv[1]
 with open("versions/latest.json") as f:
     dat = json.load(f)
 
+print(f"Update latest from {dat['latest']} to {ver}")
 dat["latest"] = ver
 with open("versions/latest.json", "w") as f:
     json.dump(dat, f)
